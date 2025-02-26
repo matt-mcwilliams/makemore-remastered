7f0e7e59fffacd9e6f6e8febd004236949226de8
Matthew McWilliams (School Laptop) <mcwilliams38229@students.apsb.org>
2025-02-25 21:55:22 -0600

    first commit

---
+++ makemore.py
@@ -0,0 +1,124 @@
+
+
+"""
+A remaster of Andrej Karpathy's makemore!
+"""
+
+
+
+# -------- IMPORTS -----------
+
+import torch
+import torch.nn.functional as F
+
+
+# ----------------------------
+# Bigram Model
+# ----------------------------
+
+class Bigram():
+
+    def __init__(self, vocab_size, generator=None):
+        self.W = torch.randn((27,27), generator=generator, requires_grad=True)
+    
+    def forward(self, xenc):
+        return xenc @ self.W
+    
+    def parameters(self):
+        return [ self.W ]
+
+    def context_length(self):
+        return 1
+    
+
+
+
+if __name__ == '__main__':
+    
+    g = torch.Generator().manual_seed(314159265)
+
+    # ---------- select model ----------
+
+    model = Bigram(27, generator=g)
+
+
+    # ---------- load words -->  xs,  ys ----------
+
+    context_length = model.context_length()
+    with open('names.txt', 'r') as file:
+
+        words = ['.'*context_length + w.strip() + '.' for w in file.readlines()]
+
+    vocab = sorted(list(set(''.join(words))))
+    stoi = {c:i for i,c in enumerate(vocab)}
+    itos = {i:c for c,i in stoi.items()}
+
+    xs = []
+    ys = []
+
+    # TODO: Allow for longer context lengths. currently this approach only works for cl = 1
+    for w in words:
+        pairs = [(x,y) for x,y in zip(w, w[1:])]
+        indexed_pairs = [(stoi[x], stoi[y]) for x,y in pairs]
+        xs += [x for x,y in indexed_pairs]
+        ys += [y for x,y in indexed_pairs]
+    
+    xs = torch.tensor(xs)
+    ys = torch.tensor(ys)
+
+
+    # ---------- begin training ----------
+    # TODO: periodically sample from the network
+
+    for epoch in range(200):
+        
+        # forward pass
+        xenc = F.one_hot(xs, num_classes=27).float()
+        logits = model.forward(xenc)
+        counts = logits.exp()
+        probs = counts / counts.sum(1, keepdim=True)
+        loss = -probs[torch.arange(probs.size(0)), ys].log().mean()
+
+        print(loss)
+
+
+        # backward pass
+        for p in model.parameters():
+            p.grad = None
+        loss.backward()
+
+
+        # update
+        for p in model.parameters():
+            p.data += -10 * p.grad
+    
+
+
+    # generate ten names
+    
+    for i in range(10):
+
+        ix = torch.tensor(0) # []
+        out = ''
+        while True:
+            xenc = F.one_hot(ix, num_classes=27).float()  # [27]
+            logits = model.forward(xenc)
+            counts = logits.exp()
+            probs = counts / counts.sum()
+
+            ix = torch.multinomial(probs, 1, replacement=True).view(1)
+
+            if ix.item()==0:
+                break
+
+            out += itos[ix.item()]
+        
+        print(out)
+            
+
+
+    
+    
+    
+    
+    pass
