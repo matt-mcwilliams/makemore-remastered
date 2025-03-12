2fddf203faeab8e476d50f9d9877c6ce327a463b
Matthew McWilliams (School Laptop) <mcwilliams38229@students.apsb.org>
2025-03-11 19:31:27 -0500

    add MLP

--- makemore.py
+++ makemore.py
@@ -12,6 +12,7 @@ import os
 import argparse
 import math
 import random
+import time
 
 import torch
 import torch.nn.functional as F
@@ -49,13 +50,14 @@ class Trigram():
         self.W = torch.randn((27**2, 27), generator=generator, requires_grad=True)
 
     def forward(self, x):
-        idx = x[...,0] + x[...,1] * 27   # [ examples x 2 (context) ]
-        # xenc = F.one_hot(idx, num_classes=27**2).float()  # [ examples ]
-        # return xenc @ self.W
+        idx = x[...,0] + x[...,1] * 27
         return self.W[idx]
     
     def parameters(self):
         return [ self.W ]
+    
+    def weights(self):
+        return [self.W]
     
     def context_length(self):
         return 2
@@ -79,54 +81,120 @@ class MultiActivatedTrigram():
     def parameters(self):
         return [ self.W ]
     
+    def weights(self):
+        return [self.W]
+    
     def context_length(self):
         return 2
 
 
+# -------------------------
+# Multi Layer Perceptron
+# -------------------------
 
+
+
+
+
+class MultiLayerPerceptron():
+
+    def __init__(self, context_length=7, hidden_neurons=200, feature_count=6, direct_connections=True, generator=None):
+        self._context_length = context_length
+        self._hidden_neurons = hidden_neurons
+        self._feature_count = feature_count
+        self._direct_connections = direct_connections
+
+        self.C = torch.randn(27, feature_count, generator=generator, requires_grad=True)
+        self.W1 = (torch.randn(feature_count*context_length, hidden_neurons, generator=generator) / math.sqrt(hidden_neurons)).clone().detach().requires_grad_(True)
+        self.b1 = torch.zeros(hidden_neurons, requires_grad=True)
+        self.W2 = (torch.randn(hidden_neurons, 27, generator=generator) / math.sqrt(27)).clone().detach().requires_grad_(True)
+        self.b2 = torch.zeros(27, requires_grad=True)
+        if direct_connections:
+            self.WD = (torch.randn(feature_count*context_length, 27, generator=generator) / math.sqrt(27)).clone().detach().requires_grad_(True)
+    
+    def forward(self, X):
+        xenc = self.C[X].view(-1, self._context_length*self._feature_count)
+        h = torch.tanh(xenc @ self.W1 + self.b1)
+        logits = h @ self.W2 + self.b2
+        if self._direct_connections:
+            logits += xenc @ self.WD
+        return logits
+    
+    def parameters(self):
+        return [ self.C, self.W1, self.b1, self.W2, self.b2 ] + ([self.WD] if self._direct_connections else [])
+    
+    def weights(self):
+        return [ self.C, self.W1, self.W2 ] + ([self.WD] if self._direct_connections else [])
+    
+    def context_length(self):
+        return self._context_length
+
+
+
+
+
+# ==========================
+# 
+# TRAINING AND RUNNING
+#
 # ==========================
 
 
 
-
-def train_model(model, X, Y, generator=None):
+def train_model(model, X, Y, Xd=None, Yd=None, generator=None):
 
     # ---------- begin training ----------
     # TODO: periodically sample from the network
     
-    epoch_count = 500
+    epoch_count = 300000
+    log_interval = 500
+    initial_learning_rate = 0.1
+    weight_decay_factor = 0.01
+
+    start_time = time.time()
     losses = []
     for epoch in range(epoch_count):
 
         # create batch
-        indicies = torch.randint(low=0, high=X.shape[0], size=(8192,), generator=generator)
+        indicies = torch.randint(low=0, high=X.shape[0], size=(256,), generator=generator)
         Xb = X[indicies]
         Yb = Y[indicies]
         
         # forward pass
         logits = model.forward(Xb).view(-1,27)
-        loss = F.cross_entropy(logits, Yb)
+        loss = F.cross_entropy(logits, Yb) + weight_decay_factor * sum([torch.mean(p**2) for p in model.weights()])
 
-        losses.append(loss.item())
-        if epoch % 20 == 0:
-            print(f'{epoch=}: {loss.item()=}')
-
-
-        # backward pass
+        losses.append(loss.log().item())
+        if epoch % log_interval == 0:
+            elapsed_time = time.time() - start_time
+            eta = ((elapsed_time) / (epoch + 1)) * (epoch_count - epoch - 1)
+            elapsed_minutes, elapsed_seconds = divmod(elapsed_time, 60)
+            eta_minutes, eta_seconds = divmod(eta, 60)
+            dev_loss = evaluate_loss(model, Xd if Xd is not None else Xb, Yd if Yd is not None else Yb)
+            train_indices = torch.randint(low=0, high=X.shape[0], size=(5000,), generator=generator)
+            train_loss = evaluate_loss(model, X[train_indices], Y[train_indices])
+            print('=====================')
+            print(f'Epoch {epoch} / {epoch_count}:')
+            print(f'')
+            print(f'Train Loss: {train_loss}')
+            print(f'Dev Loss: {dev_loss}')
+            print(f'Time: {int(elapsed_minutes)}m {int(elapsed_seconds)}s  |  ETA: {int(eta_minutes)}m {int(eta_seconds)}s')   
+            print('=====================')    # backward pass
         for p in model.parameters():
             p.grad = None
         loss.backward()
 
 
         # update
-        lr = 10**(-epoch/epoch_count) * 100 # 100 to 10, logarithmic scale
+        lr =  initial_learning_rate if epoch < epoch_count*0.6 else initial_learning_rate/10 # step learning rate decay
         for p in model.parameters():
+            # print(p.shape)
             p.data += -lr * p.grad
 
     fig, ax = plt.subplots()
     ax.plot(list(range(epoch_count)), losses)
-    ax.plot(list(range(epoch_count)), [2.57]*epoch_count)
     plt.show()
+
 
 
 def evaluate_loss(model, X, Y):
@@ -139,12 +207,17 @@ def evaluate_loss(model, X, Y):
 
 
 def load_model(model, model_name):
-    model.W = torch.load(f'models/{model_name}.pt')
+    weights = torch.load(f'models/{model_name}.pt')
+    with torch.no_grad():
+        for param, (name, weight) in zip(model.parameters(), weights.items()):
+            param.copy_(weight)
 
 
 
 def save_model(model, model_name):
-    torch.save(model.W, f'models/{model_name}.pt')
+    weights = { f'param_{i}':p for i,p in enumerate(model.parameters()) }
+    torch.save(weights, f'models/{model_name}.pt')
+
 
 
 
@@ -185,6 +258,9 @@ if __name__ == '__main__':
         
         case "ma-trigram":
             model = MultiActivatedTrigram(generator=g)
+
+        case "mlp":
+            model = MultiLayerPerceptron(generator=g)
         
         case _:
             raise Exception("Model is not availible!")
@@ -219,6 +295,7 @@ if __name__ == '__main__':
             X.append(x)
             Y.append(y)
     
+    # TODO: Refactor to use pytorch randomness to create consistancy.
     new_indicies = list(range(len(X)))
     random.shuffle(new_indicies)
     X = [X[i] for i in new_indicies]
@@ -239,7 +316,7 @@ if __name__ == '__main__':
     # ---------- train model -----------
 
     if not pretrained:
-        train_model(model, Xt, Yt, generator=g)
+        train_model(model, Xt, Yt, Xd, Yd, generator=g)
         
         print('\n===================')
         print('Training complete!')
@@ -256,20 +333,19 @@ if __name__ == '__main__':
     
     for i in range(10):
 
-        ix = torch.tensor([0]*context_length)
+        context = [0] * context_length
         out = ''
         while True:
+            ix = torch.tensor(context)
             logits = model.forward(ix)
-            counts = logits.exp()
-            probs = counts / counts.sum()
+            probs = F.softmax(logits, dim=1)
 
-            generated_idx = torch.multinomial(probs, 1, replacement=True).view(1)
+            generated_idx = torch.multinomial(probs, 1, generator=g, replacement=True).view(1)
 
             if generated_idx.item()==0:
                 break
 
             out += itos[generated_idx.item()]
-            ix[:-1] = ix[1:]
-            ix[-1] = generated_idx
+            context = context[1:] + [generated_idx.item()]
         
         print(out)
